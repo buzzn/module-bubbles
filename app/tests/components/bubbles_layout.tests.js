981b87b8736f63232f339e3e8adf3868281a254e
ChipJuggler <scsi.rdx@gmail.com>
2017-02-09 18:47:59 +0700

    test fixes

--- app/tests/components/bubbles_layout.tests.js
+++ app/tests/components/bubbles_layout.tests.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
-import { BubblesWrapper } from '../../components/bubbles_wrapper';
+import BubblesLayout from '../../components/bubbles_layout';
 import { Bubbles } from '../../components/bubbles';
 import InfoPanel from '../../components/info_panel';
 
@@ -14,22 +14,24 @@ describe('bubbles wrapper', () => {
     setData() {},
     setLoading() {},
     setLoaded() {},
+    Bubbles,
+    InfoPanel,
   };
 
   it('should render 2 InfoPanels', () => {
-    const wrapper = shallow(<BubblesWrapper { ...props } />);
+    const wrapper = shallow(<BubblesLayout { ...props } />);
     expect(wrapper.find(InfoPanel)).to.have.length(2);
   });
 
   it('should render bubbles if there is a groupId', () => {
-    const wrapper = shallow(<BubblesWrapper { ...props } />);
+    const wrapper = shallow(<BubblesLayout { ...props } />);
     expect(wrapper.find(Bubbles)).to.have.length(1);
     wrapper.setProps({ group: null });
     expect(wrapper.find(Bubbles)).to.have.length(0);
   });
 
   it('should render loading on top of bubbles in case of loading prop', () => {
-    const wrapper = shallow(<BubblesWrapper { ...props } />);
+    const wrapper = shallow(<BubblesLayout { ...props } />);
     expect(wrapper.find('.basic-loading').html()).to.have.string('z-index:-10');
     wrapper.setProps({ loading: true });
     expect(wrapper.find('.basic-loading').html()).to.have.string('z-index:10');

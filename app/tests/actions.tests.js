2fd90f4cabff2f15fcd72db67c67ab97ee1151ff
ChipJuggler <scsi.rdx@gmail.com>
2017-06-15 21:17:55 +0300

    tests removed

--- app/tests/actions.tests.js
+++ app/tests/actions.tests.js
@@ -2,23 +2,9 @@ import { expect } from 'chai';
 import { actions, constants } from '../actions';
 
 describe('bubbles actions', () => {
-  it('should create an action to set group', () => {
-    const group = 'group';
-    const expectedAction = { type: constants.SET_GROUP, group };
-    expect(actions.setGroup(group)).to.eql(expectedAction);
-  });
-
-  it('should create action to set data', () => {
-    const data = 'data';
-    const expectedAction = { type: constants.SET_DATA, data };
-    expect(actions.setData(data)).to.eql(expectedAction);
-  });
-
-  it('should create action to set loading state', () => {
-    expect(actions.loading()).to.eql({ type: constants.LOADING });
-  });
-
-  it('should create action to set loaded state', () => {
-    expect(actions.loaded()).to.eql({ type: constants.LOADED });
+  it('should create an action to set group id', () => {
+    const groupId = 'group';
+    const expectedAction = { type: constants.SET_GROUP_ID, groupId };
+    expect(actions.setGroupId(groupId)).to.eql(expectedAction);
   });
 });
